f75948505efb934d2158be5d0c5f26be04cfc5b5
thanaphon0656 <thanaphon0656@gmail.com>
2025-02-21 17:58:16 +0700

    fix server

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,13 @@ require("tsconfig-paths/register");
 
 import App from "./app";
 import validateEnv from "./utils/validateEnv";
-import recursiveDirectory from "node-recursive-directory";
+import * as recursiveDirectory from "node-recursive-directory";
 
 validateEnv();
 
 (async () => {
     try {
-        const files = await recursiveDirectory(__dirname + "/controllers/", true);
+        const files = await recursiveDirectory.default(__dirname + "/controllers/", true);
         const app = new App(files.map((file) => file.fullpath));
         app.listen();
     } catch (error) {
